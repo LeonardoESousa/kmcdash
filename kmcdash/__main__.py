7bf58e546f9c5da8e6780f741b9c2434ad87914a
TiagoSC <tiagofis96@gmail.com>
2022-10-21 11:21:05 -0300

    Update __main__.py

--- kmcdash/__main__.py
+++ kmcdash/__main__.py
@@ -5,6 +5,8 @@ import subprocess
 def main():
     path = os.path.join(os.path.dirname(__file__),"Dashboard_KMC.ipynb")
     path = r"{} ".format(path)
-    option = r'--Voila.tornado_settings={}'.format('''{'websocket_max_message_size': 209715200}''')
-    subprocess.call(['voila',path,option])
+    #option =  " --Voila.tornado_settings=\"{'websocket_max_message_size': 209715200}\" " #old option format. Keep for a while
+    option =  " --Voila.tornado_settings \"'websocket_max_message_size'=209715200\""
+    command = "voila "+path+option
+    subprocess.run(command, shell=True)
     
