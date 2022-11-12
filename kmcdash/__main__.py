094f1df8e523fb2f8dfa31759148c5bf554fa644
TSA-Cassiano <tiagofis96@gmail.com>
2022-11-12 12:57:30 -0300

    fixed export

--- kmcdash/__main__.py
+++ kmcdash/__main__.py
@@ -5,8 +5,11 @@ import subprocess
 def main():
     path = os.path.join(os.path.dirname(__file__),"Dashboard_KMC.ipynb")
     path = r"{} ".format(path)
-    #option =  " --Voila.tornado_settings=\"{'websocket_max_message_size': 209715200}\" " #old option format. Keep for a while
-    option =  " --Voila.tornado_settings \"'websocket_max_message_size'=209715200\""
+    working_dir = os.getcwd()+'/'
+    with open(os.path.dirname(__file__)+'/pathfile.txt','w') as f:
+        f.write(working_dir+'\n')
+    option =  " --Voila.tornado_settings=\"{'websocket_max_message_size': 209715200}\" " #old option format. Keep for a while
+    #option =  " --Voila.tornado_settings \"'websocket_max_message_size'=209715200\""
     command = "voila "+path+option
     subprocess.run(command, shell=True)
     
