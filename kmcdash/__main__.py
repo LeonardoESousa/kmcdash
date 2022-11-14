f7e6a5b6366db75e3821dbd1725ae90436acafeb
LeonardoESousa <leonardo.sousa137@gmail.com>
2022-11-14 11:27:57 +0100

    got rid of read file button

--- kmcdash/__main__.py
+++ kmcdash/__main__.py
@@ -2,14 +2,31 @@ import os
 import subprocess
 
 
+#def main():
+#    path = os.path.join(os.path.dirname(__file__),"Dashboard_KMC.ipynb")
+#    path = r"{} ".format(path)
+#    working_dir = os.getcwd()+'/'
+#    with open(os.path.dirname(__file__)+'/pathfile.txt','w') as f:
+#        f.write(working_dir+'\n')
+#    option =  " --Voila.tornado_settings=\"{'websocket_max_message_size': 209715200}\" " #old option format. Keep for a while
+#    #option =  " --Voila.tornado_settings \"'websocket_max_message_size'=209715200\""
+#    command = "voila "+path+option
+#    subprocess.run(command, shell=True)
+
 def main():
-    path = os.path.join(os.path.dirname(__file__),"Dashboard_KMC.ipynb")
-    path = r"{} ".format(path)
-    working_dir = os.getcwd()+'/'
-    with open(os.path.dirname(__file__)+'/pathfile.txt','w') as f:
-        f.write(working_dir+'\n')
-    option =  " --Voila.tornado_settings=\"{'websocket_max_message_size': 209715200}\" " #old option format. Keep for a while
-    #option =  " --Voila.tornado_settings \"'websocket_max_message_size'=209715200\""
-    command = "voila "+path+option
-    subprocess.run(command, shell=True)
-    
+    try:
+        path = os.path.join(os.path.dirname(__file__),"Dashboard_KMC.ipynb")
+        path = r"{} ".format(path)
+        option = r'--Voila.tornado_settings={}'.format('''{'websocket_max_message_size': 209715200}''')
+        subprocess.Popen(['voila',path,option])
+    except:
+        print('Attempting alternative!!')
+        path = os.path.join(os.path.dirname(__file__),"Dashboard_KMC.ipynb")
+        path = r"{} ".format(path)
+        working_dir = os.getcwd()+'/'
+        with open(os.path.dirname(__file__)+'/pathfile.txt','w') as f:
+            f.write(working_dir+'\n')
+        option =  " --Voila.tornado_settings=\"{'websocket_max_message_size': 209715200}\" " #old option format. Keep for a while
+        #option =  " --Voila.tornado_settings \"'websocket_max_message_size'=209715200\""
+        command = "voila "+path+option
+        subprocess.run(command, shell=True)        
