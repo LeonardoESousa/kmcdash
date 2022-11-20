e28603f83451fe31ad0e47318338aa9ccd2ba09e
TSA-Cassiano <tiagofis96@gmail.com>
2022-11-20 18:32:19 -0300

    fixing try/except with popen

--- kmcdash/__main__.py
+++ kmcdash/__main__.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-
+   
 def main():
     path = os.path.join(os.path.dirname(__file__),"Dashboard_KMC.ipynb")
     path = r"{} ".format(path)
@@ -8,10 +8,12 @@ def main():
     with open(os.path.dirname(__file__)+'/pathfile.txt','w') as f:
         f.write(working_dir+'\n')
     try:
-        option = r'--Voila.tornado_settings={}'.format('''{'websocket_max_message_size': 209715200}''')
-        subprocess.Popen(['voila',path,option])
-    except:
-        print('Attempting alternative!!')
-        option =  " --Voila.tornado_settings=\"{'websocket_max_message_size': 209715200}\" " #old option format. Keep for a while
+    	option = r'--Voila.tornado_settings={}'.format('''{'websocket_max_message_size': 209715200}''')
+    	subprocess.check_output(['voila',path,option])
+    	#subprocess.Popen(['voila',path,option])
+    except subprocess.CalledProcessError as e:
+        #option =  " --Voila.tornado_settings=\"{'websocket_max_message_size': 209715200}\" " #old option format. Keep for a while
+        option =  " --Voila.tornado_settings \"'websocket_max_message_size'=209715200\""
         command = "voila "+path+option
-        subprocess.run(command, shell=True)        
+        subprocess.run(command, shell=True)     
+
