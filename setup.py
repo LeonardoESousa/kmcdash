2f8df56e87a3aee8501c0e302789dfe04ee435d0
LeonardoESousa <leonardo.sousa137@gmail.com>
2023-10-17 12:06:31 +0200

    improved dependency

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ AUTHOR = 'Leonardo Evaristo de Sousa and Tiago de Sousa Araújo Cassiano'
 REQUIRES_PYTHON = '>=3.8.0'
 
 # What packages are required for this module to be executed?
-REQUIRED = ['matplotlib<=3.5.0', 'voila==0.3.2', 'ipywidgets==7.7.2', 'pandas', 'IPython','Ipympl==0.7.0', 'jinja2==3.0.0']
+REQUIRED = ['matplotlib<=3.5.1', 'voila==0.4.0', 'ipywidgets==8.0.6', 'pandas==1.5.2', 'IPython','Ipympl==0.9.3','jinja2==3.0.0']
 
 # What packages are optional?
 EXTRAS = {
